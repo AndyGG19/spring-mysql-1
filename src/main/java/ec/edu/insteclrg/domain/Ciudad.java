6dd57eb7e4c37e7f99449f2925d1864d74b32348
Andres Guachichullca <joseguachichullca010@gmail.com>
2022-07-07 21:24:29 -0500

    Merge remote-tracking branch 'origin/main' into main

---
+++ src/main/java/ec/edu/insteclrg/domain/Ciudad.java
@@ -0,0 +1,35 @@
+package ec.edu.insteclrg.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Ciudad {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private long id;
+	@Column(nullable = false)
+	private String nom_ciudad;
+		
+	@ManyToOne
+	@JoinColumn
+	private Provincia provincia;
+
+	
+	
+	
+
+	
+}
